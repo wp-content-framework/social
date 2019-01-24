8093bb65c5b81bc3c543a92eca2a8113c8aba052
technote <technote.space@gmail.com>
2019-01-24 20:30:30 +0900

    first commit

---
+++ package_social.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * WP_Framework Package Social
+ *
+ * @version 0.0.1
+ * @author technote-space
+ * @since 0.0.1
+ * @copyright technote-space All Rights Reserved
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @link https://technote.space
+ */
+
+namespace WP_Framework;
+
+if ( ! defined( 'WP_CONTENT_FRAMEWORK' ) ) {
+	exit;
+}
+
+/**
+ * Class Package_Social
+ * @package WP_Framework
+ */
+class Package_Social extends Package_Base {
+
+	/**
+	 * initialize
+	 */
+	protected function initialize() {
+
+	}
+
+	/**
+	 * @return int
+	 */
+	public function get_priority() {
+		return 20;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_configs() {
+		return [
+			'filter',
+			'map',
+		];
+	}
+}
